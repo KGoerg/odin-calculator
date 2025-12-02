d53fad86a23fc4de2845003d6649217f657cfb3d
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-12-02 10:46:13 -0600

    Create "operate" function
    This pulls in either the add, subtract, multiply, or divide functions as helper functions.

--- javascript.js
+++ javascript.js
@@ -13,3 +13,21 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 };
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber) {
+    if (operator === "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operator === "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === "/") {
+        return divide(firstNumber, secondNumber);
+    }
+};
+
+console.log(operate(7,"*",2));
