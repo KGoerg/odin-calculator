d86af274d7ffaf4df5c9cabc16bafe0a098bae09
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-12-19 15:00:15 -0600

    Create getOperator function

--- javascript.js
+++ javascript.js
@@ -17,6 +17,8 @@ function divide (num1, num2) {
 let firstNumber;
 let operator = "";
 let secondNumber;
+let firstNumberArray = [];
+let secondNumberArray = [];
 
 function operate(num1, operator, num2) {
     if (operator === "+") {
@@ -31,26 +33,51 @@ function operate(num1, operator, num2) {
 };
 
 const numberButtons = document.querySelectorAll(".button.number");
-const display = document.querySelector("#display");
-let firstNumberArray = [];
-let secondNumberArray = [];
+const operatorButtons = document.querySelectorAll(".button.operator");
+const display = document.querySelector(".display");
+//Makes display unable to take in keyboard commands at this time.
+display.readOnly = true;
 
 function getFirstNumber() {
+    firstNumber;
     numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
         firstNumberArray.push(button.textContent);
         number = Number(firstNumberArray.join(""));
         firstNumber = number;
+        display.value = firstNumber;
         console.log(`firstNumber = ${firstNumber}`);
     })});
+    return firstNumber;
 };
 
 function getSecondNumber() {
     numberButtons.forEach((button) => {
+    secondNumber;
     button.addEventListener("click", () => {
         secondNumberArray.push(button.textContent);
         number = Number(secondNumberArray.join(""));
         secondNumber = number;
-        console.log(`firstNumber = ${secondNumber}`);
+        display.value = secondNumber;
+        console.log(`secondNumber = ${secondNumber}`);
     })});
+    return secondNumber;
 };
+
+function getOperator() {
+    operatorButtons.forEach((button) => {
+        operator;
+        button.addEventListener("click", () => {
+            operator = button.textContent;
+            display.value = operator;
+        })
+        return operator;
+    })
+}
+
+getOperator();
+
+const clearButton = document.querySelector("#clear")
+clearButton.addEventListener("click", () => {
+    location.reload();
+});
