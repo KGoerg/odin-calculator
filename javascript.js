24840692952c390218c1570b8284b43a311dd6fd
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-12-19 14:22:33 -0600

    Complete steps 1-3 and create first & second number functions

--- javascript.js
+++ javascript.js
@@ -1,112 +1,56 @@
-let number;
-let firstNumberArray = [];
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
 let firstNumber;
-let secondNumberArray = [];
+let operator = "";
 let secondNumber;
-let operator;
-let result;
 
-//Function expressions
-let getNumbers;
-let getUserOperator;
-
-function add(num1, num2) {
-    result = num1 + num2;
-    firstNumber = result;
-    secondNumberArray = [];
-    return roundToTenDecimals(result);
-};
-
-function subtract(num1, num2) {
-    result = num1 - num2;
-    firstNumber = result;
-    secondNumberArray = [];
-    return roundToTenDecimals(result);
-};
-
-function multiply(num1, num2) {
-    result = num1 * num2;
-    firstNumber = result;
-    secondNumberArray = [];
-    return roundToTenDecimals(result);
-};
-
-function divide(num1, num2) {
-    if (num2 === 0) {
-        return "Really?";
+function operate(num1, operator, num2) {
+    if (operator === "+") {
+        return add (num1, num2);
+    } else if (operator === "-") {
+        return subtract (num1, num2);
+    } else if (operator === "x") {
+        return multiply (num1, num2);
     } else {
-    result = num1 / num2;
-    firstNumber = result;
-    secondNumberArray = [];
-    return roundToTenDecimals(result);
-}};
-
-function operate(num1, action, num2) {
-    if (action === "+") {
-        return add(num1, num2);
-    } else if (action === "-") {
-        return subtract(num1, num2);
-    } else if (action === "X") {
-        return multiply(num1, num2);
-    } else if (action === "/") {
-        return divide(num1, num2);
+        return divide (num1, num2);
     }
 };
 
-const display = document.querySelector(".display");
-//Makes display unable to take in keyboard commands at this time.
-display.readOnly = true;
-
 const numberButtons = document.querySelectorAll(".button.number");
-const operatorButtons = document.querySelectorAll(".button.operator");
+const display = document.querySelector("#display");
+let firstNumberArray = [];
+let secondNumberArray = [];
 
-getUserOperator = function() {
-    operatorButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        display.value = button.textContent;
-        if (secondNumber !== undefined && result === undefined) {
-            display.value = operate(firstNumber, operator, secondNumber);
-        }
-        operator = button.textContent;
-        console.log(`operator = ${operator}`);
-    })
-})};
-
-getUserOperator();
-
-function roundToTenDecimals(num) {
-    return Number(Math.round(num + "e+10") + "e-10");
-};
-
-getNumbers = function() {
+function getFirstNumber() {
     numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        if (operator === undefined) {
-            firstNumberArray.push(button.textContent);
-            number = Number(firstNumberArray.join(""));
-            display.value = number;
-            firstNumber = number;
-            console.log(`firstNumber = ${firstNumber}`);
-        } else if (firstNumber !== undefined) {
-            secondNumberArray.push(button.textContent);
-            number = Number(secondNumberArray.join(""));
-            display.value = number;
-            secondNumber = number;
-            console.log(secondNumberArray);
-            console.log(`secondNumber = ${secondNumber}`)
-        }
-    })
-})};
+        firstNumberArray.push(button.textContent);
+        number = Number(firstNumberArray.join(""));
+        firstNumber = number;
+        console.log(`firstNumber = ${firstNumber}`);
+    })});
+};
 
-getNumbers();
-
-const clearButton = document.querySelector("#clear")
-clearButton.addEventListener("click", () => {
-    location.reload();
-});
-
-const equalsButton = document.querySelector("#equals");
-equalsButton.addEventListener("click", () => {
-    display.value = operate(firstNumber, operator, secondNumber);
-    console.log(`result = ${result}`);
-});
+function getSecondNumber() {
+    numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        secondNumberArray.push(button.textContent);
+        number = Number(secondNumberArray.join(""));
+        secondNumber = number;
+        console.log(`firstNumber = ${secondNumber}`);
+    })});
+};
